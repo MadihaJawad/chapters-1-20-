cc3df121d54f9b17ad0b29e04fccb248aeb791d1
MadihaJawad <madihanaveed306@gmail.com>
2023-05-20 12:17:36 +0500

    Done

---
+++ app.js
@@ -0,0 +1,704 @@
+//////////////////////////////////// chapter 1(Alerts)///////////////////////////////
+// Question 1
+// Answer
+// alert("Madiha");
+// alert("Naveed");
+// alert("madihanaveed306@gmail.com");
+// alert("0312495839589");
+// alert("jaad456");
+// Question 2 
+// Answer
+// alert("you're learning javascript!");
+// Question 3 
+// Answer
+// alert("I am leaning javascript.");
+////////////////////////////// chapter 2 (Variables for string)/////////////////////////
+// Question 1
+// Answer
+// var myFavoritesColor = "blue";
+
+// Question 2
+// Answer
+// var myFavoritesCountry;
+// myFavoritesCountry = "Pakistan";
+
+// Question 3
+// Answer
+// var teamName = "Quaid";
+// alert("teamName");
+
+// Question 4
+// Answer
+// var bestMan = "Charlie";
+// bestMan = "Leo";
+
+////////////////////// chapter 3 (variables for numbers)////////////////////////////////////
+// Question 1 
+// Answer
+// var caseQty = 10;
+
+// Question 2
+// Answer
+// caseQty = 144;
+
+// Question 3
+// Answer
+// var num = "9";
+
+// Question 4 
+// Answer
+// var sum = 2+2
+
+// Question 5
+// Answer
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+
+// Question 6
+// Answer
+// var num = 3;
+// var num1 = 5;
+// var total= num + num1;
+// alert(total)
+
+// /////////////chapter 4 (variables names legal and illegal)///////////////////////////////
+// Question 1
+// Answer
+// var productCost = 3.45;
+
+// Question 2 
+// Answer
+// var nameOfBand;
+
+// Question 3 
+// Answer
+// var myFavoritesCountry = 
+
+// Question 4
+// Answer
+// var first = Ali;
+// var last = Shah;
+// var combineName = first + last ;
+// alert(combineName);
+
+// Question 5
+// Answer/
+// lligal variables names
+// myvar = "John"
+// my_var = "John"
+// _my_var = "John"
+// myVar = "John"
+// MYVAR = "John"
+// myvar2 = "John"
+
+// Illegal variable names:
+// 2myvar = "John"
+// my-var = "John"
+// my var = "John"
+
+
+// Chapter 5 (Math Expression)
+// Question 1
+// Answer
+// var num = 10;
+// var anotherNum = 3;
+// var remainder = num % anotherNum;
+// alert(remainder);
+
+// Question 2 
+// Answer
+// var num1 = 20;
+// var num2 = 6;
+// var total = num1 % num2;
+// alert(total);
+
+// Question 3 
+// Answer
+// var largeNum = 1000*2000;
+
+// Question 4
+// Answer
+// var variable1 = 10;
+// var variable2 = 5;
+// var result = variable1-variable2;
+// alert(result);
+
+// Question 5
+// Answer
+// var divided = 10;
+// var divisor = 3;
+// var result = divided / divisor;
+// alert(result);
+
+// Question 6 
+// Answer
+// var num1 = 3;
+// var num2 = 3;
+// var result = num1 * num2;
+// alert("The result of the multiplication is " + result);
+
+
+// chapter 6 ( Math Expression II)
+
+// Question 1
+// Answer
+// var x = x + 1;
+// alert(x);
+
+// Question 2
+// Answer
+// var x = 100;
+// x = x-1;
+// console.log(x);
+
+// Question 3 
+// Answer
+// var x = 50;
+// var y = x++;
+// console.log(y)
+
+// Question 4 
+// Answer
+// var y = 50;
+// var z = --y;
+// console.log(z);
+
+// Question 5
+// Answer
+// var num = 10 ;
+// var newNum = --num;
+// console.log(newNum);
+
+
+
+// Question 6 
+// Answer
+// var num = 10;
+// var newNum = 1++;
+// console.log(newNum);
+
+
+// Question 7 
+// Answer
+// var num = 8;
+// num++
+// alert("The new value is: " + num);
+
+
+
+////////////////////// Chapter 7 (Math Epression III)////////////////////////////
+// Question 1
+// Answer
+// var calculateNum = 2+(2*6);
+// alert(calculateNum)
+
+// Question 2
+// Answe
+// var calculatedNum = (2+2)*6;
+// alert(calculatedNum);
+
+// Question 3 
+// Answer
+// var  calculatedNum = (2+2)*(4+2);
+// alert(calculatedNum);
+
+// Question 4
+// Answer
+// var calculatedNum = ((2+2)*4)+ 2;
+// alert(calculatedNum);
+
+// Question 5 
+// Answer
+// var cost = (2+2)*4+10;
+// alert(cost)
+
+
+// Question 6 
+// Answer
+// var cost = (2+(2*4)+10)
+// alert(cost);
+
+
+
+// Question 7 
+// Answer
+// var pressure = (4 / 2)*4;
+// console.log(pressure);
+
+//////////////////////////// chapter 8 (Concatenation text string)/////////////////
+
+// Question 1
+// answer
+// var num ="2"+"2";
+// alert("The sum of " + num)
+
+// Question 2
+// answer
+// var message ="Hello";
+// var names = "Dolly";
+// alert(message + names)
+
+// Question 3
+// answer
+// alert("33" + 3)
+
+// Question 4
+// answer
+// var message1 = "Pakistan";
+// var message2 = "Zindabad";
+// var result = message1 + message2;
+// alert(result);
+
+// Question 5
+// answer
+// var num = 4;
+// var num1 = 5;
+// var total = num+ num1;
+// alert(total);
+
+// Question 6
+// answer
+// var str1 = "Hello";
+// var str2 = "World";
+// var string = str1 + str2;
+// alert(string);
+
+//////////////////////////// Chapters(prompt)/////////////////////////////////////////////////////
+// Question 1
+// answer
+// var nam = prompt("Enter first name");
+// alert(nam);
+
+// Question 2
+// answer
+// var message = "Country?";
+// var Answer =  "China";
+// var country = prompt(message + Answer)
+// document.write(country);
+
+// Question 3
+// answer
+// var yourName =prompt("Enter your name");
+
+//Question 4
+// answer
+// var message = "Please enter your name";
+// var defaultInput =  "Ali shah";
+// var userInput = prompt(message,defaultInput);
+// document.write(userInput);
+
+// Question 5
+// answer
+// var question = "Your species?";
+// var defaultInput = "jawad shah";
+// var spec = prompt(question,defaultInput);
+// alert(spec); 
+
+// Question 6
+// answer
+// var message = "What is you favorite color";
+// var defaultInput = "blue";
+// var response = prompt(message,defaultInput);
+// alert(response);
+
+// /////////////////////chapter 10(if Statment)////////////////////////////////////////
+// Question 1
+// answer
+// var city = "karachi";
+// if(city ="karachi"){
+    // console.log("The city of lights")
+// }
+
+// Question 2
+// answer
+ 
+// Question 3
+// answer
+// var zipCode = "10010"
+// if (zipCode === "10010" ){
+    // alert("Karchi");
+// }
+// else{
+    // alert("Please write correct city ")
+// }
+
+// Question 4
+// Answer
+// var x = 1;
+// if(x === 1){
+//    
+// }
+// console.log(x)
+
+///////////////////// chapter 11(comparison operator)//////////////////////////////////////////////
+// Question 1
+// answer
+// if(variable1 !== variable2){
+// }
+
+// Question 2
+// answer
+// var a = 1
+// if(1 >= 0){
+// }
+
+// Question 3
+// answer
+// if(variable1 !== 55 ){
+    // variable = 10 
+// }
+
+// Question 4
+// answer
+// variable1 = 6
+// variable2 = 3
+// if( variable1 !== variable2 ){
+    // console.log("Congratulation")
+// }
+
+
+// Question 5
+// answer
+// var firstName = prompt("first Name")
+// if(firstName !== anotherName){
+    // alert("nomatch")
+// }
+
+// /////////////////Chapter 12 (if...else and else if statements)///////////////////////
+// Question 1
+// answer
+// var value1 = 8;
+// var value2 = 5;
+// if (value1 >= value2 ){
+    // alert("The value1 is greater then value2")
+// }
+// else{
+    // alert("The value1 is less thenn value2")
+// }
+
+
+// Question 2
+// answer
+// var marks = +prompt("Enter your marks")
+// if(marks >= 80 ){
+    // alert("A")
+// 
+// }
+// else if(
+    // marks >=70){
+//    alert("B")
+    // }
+// else if (marks >= 60){
+    //  alert("C")
+// }
+// else if (marks <=50){
+    // alert("fail")
+// }
+
+
+    // Question 3
+// answer
+// var a = 5
+// if (a === 10 ){
+//     alert("a is 10");
+// }
+// else{
+//     alert("The vale of a is " + a)
+// }
+
+
+// Question 4
+// answer 
+// var inputCity = ["Karachi" ,"Lahore", "Islamabad" ," Peshawar"];
+// var cityName = prompt("Enter a city name");
+// if(inputCity === cityName ){
+//     alert("acknowledging it is Karachi  ")
+// }
+// else if  (inputCity === cityName){
+//     alert("It is Lahore")
+// }
+// else {
+    // alert("It is another city ")
+// }
+
+//////////// Chapter 13 (Testing sets of conditions)////////////////////////////////////////////
+// Question 1
+// answer
+// if(a == b && c == d){
+// }
+
+// Question 2
+// answer
+// if(a == b && c != d){
+// }
+ 
+// Question 3
+// answer
+// name == "Hamza" || "Arsalan"
+// if(Hamza || Arsalan &&  age < 60){
+// }
+
+// Question 4
+// answer
+// var num1 = 4;
+// var num2 = 5;
+// if(num1 < num2 || num1 > num2){
+    // alert("first variable is less then or greater then the second variable")
+// }
+
+// Question 5
+// answer
+// var firstName = Abdul;
+// var secondName = Rahman;
+// var result = prompt("Enter your first name" + "Enter your second name"){
+    // alert(result)
+// }
+
+/////////////////////// Chapter 14 (If statements nested)//////////////////////////////////////
+// Question 1
+// answer
+
+
+// Question 2
+// answer
+// if(a === 1){
+    // if(c=== "Max"){
+        // alert("OK");
+    // }
+// }
+
+// Question 3
+// answer
+// if(a===1){
+    // if(c==="Mark"){
+        // alert("OK")
+    // }
+// }
+
+// Question 4
+// answer
+// var nam1 = 3;
+// var num2 = 3;
+// if(nam1 === num2){
+//     if(nam1 <= num2){
+//       alert("Both conditions passed")
+//     }
+// } 
+
+
+////////////////////// Chapter 15 (Array I)///////////////////////////////////////////////
+// Question 1
+// answer
+// var numArr =[];
+
+// Question 2
+// answer
+// var city = ["Karachi"];
+
+// Question 3
+// answer
+// var arr = ["h","i","j","k"];
+// alert(arr[2]);
+
+// Question 4
+// answer
+// var arr1 =  ["h","i","j","k","l","m","n","o"];
+// var length = arr1.length;
+// alert(length);
+
+// Question 5
+// answer
+// var arr = ["lion"];
+// arr[1] = "Monkey";
+// alert(arr[1]);
+
+
+//////////////////// Chapter 16 (Array II)///////////////////////////////////////
+// Question 1
+// answer
+// var arr = ["Blue"];
+// arr.push("green");
+// console.log(arr)
+
+
+
+// Question 2
+// answer
+// var  Alphabet = ["h","i","j","k"];
+// Alphabet.pop()
+// console.log(Alphabet);
+
+// Question 3
+// answer
+// var  Alphabet = ["h","i","j","k"];
+// Alphabet.push(o)
+// console.log(Alphabet);
+
+
+//////////////////////// Chapter 16 (Array III)//////////////////////////////////////////////////
+// Question 1
+// // answer
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift();
+// console.log(sizes);
+
+// Question 2
+// // answer
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift(2,4,6);
+// console.log(sizes);
+
+// Question 3
+// // answer
+// arr = [6];
+// arr.unshift(5);
+// alert(arr)
+
+// Question 4
+// // answer
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2,0,"L");
+// console.log(sizes);
+
+// Question 5
+// // answer
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.slice(0,3);
+// var newSizes = sizes.slice(0,3);
+// console.log(newSizes)
+
+// Question 6
+// // answer
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 3,"Tiger","Lion");
+// console.log(pets)
+
+// Question 7
+// // answer
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1,2);
+// console.log(pets)
+
+// Question 8
+// // answer
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// pets = pets.slice(3,5);
+// console.log(pets)
+
+
+// ///////////////Chapter 17 - 20 (for Loops)/////////////////////////////////////////
+// Question 1
+// // answer
+// for (let i = 0; i < 10 ; i++) {
+// }
+
+
+// Question 2
+// // answer
+// for(let i = 0; i<= 12; i++ ){
+    // var nam = "Jawad";
+    // console.log("my name is " + nam);
+// }
+
+// Question 3
+// // answer
+// for(var i = 0 ; i<= 4 ; i++){
+//     let color =  "Pakistan";
+//     console.log(color)
+// }
+
+
+
+// Question 4
+// // answer
+// for( let counter = 0; counter < 100 ; counter++){
+
+// }
+
+
+// Question 5
+// // answer
+// for (var i = 3; i > 0; i--){
+
+// }
+
+
+// Question 6
+// // answer
+// var numbers = ["1","2","3","4","5"];
+// for(var i = 0 ; i <numbers.length ; i++){
+    // document.write(numbers)
+// }
+
+
+// Question 7
+// // answer
+// boolean "flag" = true
+
+
+// Question 8
+// // answer
+// var pets = ["dog","cat","duck"];
+// for(var i = 0; i < pets.length; i++){
+//     document.write(pets)
+// }
+
+
+// Question 9
+// // answer
+// for(var i = 0; i < 10 ; i++){
+//     if(i == 1){
+//         console.log("counter" + i)
+//     }
+// }
+
+
+// Question 10
+// // answer
+// I don't understand
+
+
+
+
+// Question 11
+// // answer
+// var matchFound = false;
+// for(var i = 0; i < list.length;i++){
+//     if(userInput===list[i]){
+//         alert("Match found");
+//         matchFound=true;
+//         break
+//     }
+// };
+
+
+// Question 12
+// // answer
+
+// var firstArr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+// for(var i= 0; i< 5; i++){
+//   console,log(firstArr[i]);
+//   for(var j = 0; j < 3 ; j++){
+//     console.log(i,j)
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
